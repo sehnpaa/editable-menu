3e8748fe1585d5827081fb9c7f760332d23c013c
Peter Andersson <sehnpaa@gmail.com>
2014-06-14 21:05:49 +0200

    Add subitems

--- app/routes/application.js
+++ app/routes/application.js
@@ -7,7 +7,14 @@ export default Ember.Route.extend({
 var data = {
     items: [{
         id: 0,
-        title: "title0"
+        title: "title0",
+        subitems: [{
+            id: 0,
+            title: "sub1",
+        }, {
+            id: 1,
+            title: "sub2"
+        }]
     }, {
         id: 1,
         title: "title1"
